d65706ef18b5d7211e3c717907020501578c8abf
saeed <saeed.jalili.fard@gmail.com>
2026-07-20 08:20:16 +0330

    5454

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,29 +1,50 @@
-import { getRequestConfig } from "next-intl/server";
-import { hasLocale } from "next-intl";
-import { routing } from "./routing";
+import { getRequestConfig } from 'next-intl/server';
+import { routing } from './routing';
+
+function deepMerge(...objects: any[]) {
+  const isObject = (obj: any) => obj && typeof obj === 'object' && !Array.isArray(obj);
+  return objects.reduce((prev, obj) => {
+    Object.keys(obj).forEach(key => {
+      if (isObject(prev[key]) && isObject(obj[key])) {
+        prev[key] = deepMerge(prev[key], obj[key]);
+      } else {
+        prev[key] = obj[key];
+      }
+    });
+    return prev;
+  }, {});
+}
 
 export default getRequestConfig(async ({ requestLocale }) => {
-  const requested = await requestLocale;
+  let locale = await requestLocale;
+  if (!locale || !routing.locales.includes(locale as any)) {
+    locale = routing.defaultLocale;
+  }
 
-  const locale = hasLocale(routing.locales, requested)
-    ? requested
-    : routing.defaultLocale;
+  const rootMessages = (await import(`../../messages/${locale}.json`)).default;
+  
+  const loadModuleMsg = async (path: string) => 
+    (await import(`../modules/${path}/messages/${locale}.json`).catch(() => ({ default: {} }))).default;
 
-  const globalMessages = (await import(`../../messages/${locale}.json`))
-    .default;
-  const heroFlowMessages = (
-    await import(`../modules/hero-flow/messages/${locale}.json`)
-  ).default;
-  const aparatMessages = (
-    await import(`../modules/aparat/messages/${locale}.json`)
-  ).default;
+  const flowMessages = await loadModuleMsg('claude-flow');
+  const dashboardMessages = await loadModuleMsg('dashboard');
+  const authMessages = await loadModuleMsg('auth');
+  const homeMessages = await loadModuleMsg('home');
+  const welcomeMessages = await loadModuleMsg('welcome');
+  const aparatMessages = await loadModuleMsg('aparat');
+  const heroFlowMessages = await loadModuleMsg('hero-flow');
 
   return {
     locale,
-    messages: {
-      ...globalMessages,
-      ...heroFlowMessages,
-      Aparat: aparatMessages,
-    },
+    messages: deepMerge(
+      rootMessages,
+      flowMessages,
+      dashboardMessages,
+      authMessages,
+      homeMessages,
+      welcomeMessages,
+      aparatMessages,
+      heroFlowMessages
+    )
   };
 });
